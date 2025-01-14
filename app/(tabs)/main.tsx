39e020af21069fb50c20378c8d9c297e9c870495
Harry Manley <HarryLFManley@outlook.com>
2025-01-14 14:05:58 +0000

    Added Events Page

---
+++ app/(tabs)/main.tsx
@@ -0,0 +1,11 @@
+import Header from "../header";
+import EventList from "../../components/eventsList";
+
+export default function HomeScreen() {
+  return (
+    <>
+      <Header />
+      <EventList />
+    </>
+  );
+}
