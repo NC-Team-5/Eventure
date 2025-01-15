36d7f78a3af7a8f89f8d7d106763e3042ff6d34d
Harry Manley <HarryLFManley@outlook.com>
2025-01-15 13:42:37 +0000

    Events Page

--- app/(auth)/home.tsx
+++ app/(auth)/home.tsx
@@ -1,6 +1,3 @@
-import { StyleSheet } from "react-native";
-import { ThemedText } from "@/components/ThemedText";
-import { Text } from "react-native";
 import Header from "../header";
 import EventList from "../../components/eventsList";
 import React from "react";
@@ -8,29 +5,8 @@ import React from "react";
 export default function HomeScreen() {
   return (
     <>
-      <ThemedText>Sign in with your email and password.</ThemedText>
-      <Text>Hello</Text>
       <Header />
       <EventList />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-});
