1590caff0cda4aa29492dbdf49f705af3cbcb6f5
chris-poed <christianpoed@gmail.com>
2025-01-16 17:28:38 +0000

    add ProfilePic imageviewer component

---
+++ components/profilePage/ProfilePic.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+import { Image, type ImageSource } from "expo-image";
+
+type Props = {
+  imgSource: ImageSource;
+  selectedImage?: string;
+};
+
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+  const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
+
+  return <Image source={imageSource} style={styles.image} />;
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 250,
+    borderRadius: 18,
+    alignItems: "center",
+  },
+});
