ff789e182f8ea713bd53709a30c10400f28b197b
Alex Kontis <alex@alexkontis.co.uk>
2025-01-21 15:45:49 +0000

    merge contender

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -73,16 +73,6 @@ export default function TabLayout() {
           ),
         }}
       />
-      <Tabs.Screen
-        name="event"
-        options={{
-          href: null,
-          title: "Event",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="calendar.circle" color={color} />
-          ),
-        }}
-      />
     </Tabs>
   );
 }
